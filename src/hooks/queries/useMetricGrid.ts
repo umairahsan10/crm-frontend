7088bb3d329f3fb98f5eedf5b2e328b6be7f5fb1
ghalibzaidi110 <ghalibzaidi115@gmail.com>
2025-11-14 05:09:31 +0500

    profile all changes and new design after build

--- src/hooks/queries/useMetricGrid.ts
+++ src/hooks/queries/useMetricGrid.ts
@@ -2,7 +2,6 @@ import { useQuery } from '@tanstack/react-query';
 import { useMemo } from 'react';
 import { getMetricGridApi } from '../../apis/dashboard';
 import { transformMetricGridResponse } from '../../utils/metricTransform';
-import type { MetricData } from '../../types/dashboard';
 import type { MetricGridApiResponse } from '../../apis/dashboard';
 import { useAuth } from '../../context/AuthContext';
 
